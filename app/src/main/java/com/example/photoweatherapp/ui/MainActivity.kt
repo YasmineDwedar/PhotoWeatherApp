78d1833c5f60fbeea510c5b08bb1e72b0c0fcbfe
YasmineDwedar <yasminedwedar30@gmail.com>
2021-04-05 12:13:30 +0200

    setup project  packages

--- app/src/main/java/com/example/photoweatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/photoweatherapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.photoweatherapp
+package com.example.photoweatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.photoweatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
