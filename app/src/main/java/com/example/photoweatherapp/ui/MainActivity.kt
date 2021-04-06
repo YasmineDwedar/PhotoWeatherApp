e82df23cca65dab49c5e0cf40b81c68c28613ba5
YasmineDwedar <yasminedwedar30@gmail.com>
2021-04-06 23:59:04 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/photoweatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/photoweatherapp/ui/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.photoweatherapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.photoweatherapp.R
+import com.example.photoweatherapp.databinding.ActivityMainBinding
+import com.example.photoweatherapp.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun setListeners() {
+
+    }
+
+    override fun setObservers() {
+    }
+
+    override fun initializeViewModel() {
     }
 }
