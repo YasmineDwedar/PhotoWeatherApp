3327f7f38556758b22a73ce7215d699ba17c0da3
YasmineDwedar <yasminedwedar30@gmail.com>
2021-04-07 20:43:07 +0200

    Update WeatherInfo.kt

--- app/src/main/java/com/example/photoweatherapp/data/local/WeatherInfo.kt
+++ app/src/main/java/com/example/photoweatherapp/data/local/WeatherInfo.kt
@@ -2,15 +2,22 @@ package com.example.photoweatherapp.data.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.photoweatherapp.models.Main
+import com.example.photoweatherapp.models.Sys
 import com.example.photoweatherapp.models.WeatherResponse
+import com.example.photoweatherapp.models.Wind
 
 /**
  * Created by Yasmine on April,2021
  */
 @Entity(tableName = "weather")
 data class WeatherInfo(
-    var weatherResponse: WeatherResponse,
+    val feels_like: Double,
+    val temp: Double,
+    val name: String,
+    val sys: Sys,
+    val wind: Wind,
     var img:String,
-    @PrimaryKey
-    var id:Int
+    @PrimaryKey(autoGenerate = true)
+    var id:Int=0
 )
