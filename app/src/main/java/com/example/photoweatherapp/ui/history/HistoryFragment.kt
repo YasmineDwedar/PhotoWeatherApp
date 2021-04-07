1a2543ea70705e966665cfb77b7b82880e2a3054
YasmineDwedar <yasminedwedar30@gmail.com>
2021-04-07 21:47:04 +0200

    Update HistoryFragment.kt

--- app/src/main/java/com/example/photoweatherapp/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/photoweatherapp/ui/history/HistoryFragment.kt
@@ -1,26 +1,16 @@
 package com.example.photoweatherapp.ui.history
 
-import android.Manifest
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.photoweatherapp.R
-import com.example.photoweatherapp.data.local.WeatherInfo
 import com.example.photoweatherapp.databinding.FragmentHistoryBinding
-import com.example.photoweatherapp.models.Sys
-import com.example.photoweatherapp.models.Wind
 import com.example.photoweatherapp.ui.base.BaseFragment
-import com.example.photoweatherapp.ui.photo.PhotoFragmentViewModel
-import com.example.photoweatherapp.util.Constants
-import com.example.photoweatherapp.util.LocationProvider
 import kotlinx.coroutines.InternalCoroutinesApi
-import pub.devrel.easypermissions.EasyPermissions
 
 
 class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_history) {
@@ -39,7 +29,6 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_h
 
     override fun setListeners() {
         binding?.fabTakePhoto?.setOnClickListener {
-//            openTheCamera()
             openCameraAfterPermissionGranted()
             getUserCurrentLocation()
 
@@ -49,7 +38,6 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_h
     }
 
     override fun openTheCamera() {
-//        findNavController().navigate(R.id.action_historyFragment_to_photoFragment)
         findNavController().navigate(R.id.action_historyFragment_to_captureFragment)
     }
 
